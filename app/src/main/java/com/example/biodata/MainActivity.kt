284698d432df2064c2d56ca097717797393f04d8
abelherl <abangabel02@gmail.com>
2020-05-05 15:46:54 +0700

    Updated home screen OnClickListener testing

--- app/src/main/java/com/example/biodata/MainActivity.kt
+++ app/src/main/java/com/example/biodata/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.biodata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bt_simpan = findViewById<Button>(R.id.bt_simpan)
+
+        bt_simpan.setOnClickListener { goToHome() }
+    }
+
+    private fun goToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
     }
 }
